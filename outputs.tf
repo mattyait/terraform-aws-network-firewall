db9f88f4b07f54c866c4e4551ae3fd7dc8f9b7be
mattyait <mattyait@gmail.com>
2023-01-17 13:08:38 +1100

    fixing the syntax for build warning

--- outputs.tf
+++ outputs.tf
@@ -10,5 +10,5 @@ output "arn" {
 
 output "endpoint_id" {
   description = "Created Network Firewall endpoint id"
-  value       = flatten(aws_networkfirewall_firewall.this.firewall_status[*].sync_states[*].*.attachment[*])[*].endpoint_id
+  value       = flatten(aws_networkfirewall_firewall.this.firewall_status[*].sync_states[*].attachment[*])[*].endpoint_id
 }
