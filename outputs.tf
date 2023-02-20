622f921af1d43e67360d5e9da4e6b661378f84c4
mattyait <mattyait@gmail.com>
2023-02-20 13:50:26 +1100

    fix: output returning firewall state and firewall endpoint with AZ as map

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,18 @@ output "endpoint_id" {
   description = "Created Network Firewall endpoint id"
   value       = flatten(aws_networkfirewall_firewall.this.firewall_status[*].sync_states[*].attachment[*])[*].endpoint_id
 }
+
+output "subnet_id" {
+  description = "Created Network Firewall subnet id"
+  value       = flatten(aws_networkfirewall_firewall.this.firewall_status[*].sync_states[*].attachment[*])[*].subnet_id
+}
+
+output "sync_states" {
+  description = "Created Network Firewall states"
+  value       = flatten(aws_networkfirewall_firewall.this.firewall_status[*].sync_states[*])
+}
+
+output "endpoint_id_az" {
+  description = "Map value with Availability Zone and Firewall endpoint id"
+  value       = { for val in flatten(aws_networkfirewall_firewall.this.firewall_status[*].sync_states[*]) : val.availability_zone => val.attachment[0].endpoint_id }
+}
